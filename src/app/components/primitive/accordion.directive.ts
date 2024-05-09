d2db14f935317b2f65d874936d142653cb2f8ce4
navya.vanga <navya.vanga@techwave.net>
2024-05-09 11:40:27 +0530

    removed unneccessary code from accordion component

--- src/app/components/primitive/accordion.directive.ts
+++ src/app/components/primitive/accordion.directive.ts
@@ -1,5 +1,4 @@
-import { ContentChildren, Directive, ElementRef,  HostListener, Input, QueryList, Renderer2 } from '@angular/core';
-import { AccordionItemDirective } from './accordion-item.directive';
+import { Directive, ElementRef,  HostListener, Input, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appAccordion]'
@@ -9,11 +8,7 @@ export class AccordionDirective {
   @Input() bg_color!: string;
   @Input() disabled!: boolean;
  
-  @ContentChildren(AccordionItemDirective) items!: QueryList<AccordionItemDirective>;
-
-  constructor(private el: ElementRef, private renderer: Renderer2) { 
-    
-  }
+  constructor(private el: ElementRef, private renderer: Renderer2) { }
 
 
   @HostListener('mouseenter') onMouseEnter() {   
@@ -43,22 +38,21 @@ export class AccordionDirective {
     this.renderer.setStyle(this.el.nativeElement, 'align-items', 'center'); 
     this.renderer.setStyle(this.el.nativeElement, 'border', '1px solid #B5B5B5');   
     this.renderer.setStyle(this.el.nativeElement, 'font-size', '18px'); 
-
     const btn_svg = this.el.nativeElement.querySelector('.btn_svg');
     this.renderer.setStyle(this.el.nativeElement, 'cursor', 'pointer');
     
-    
-        if(this.bg_color == "white") {
+      if(this.bg_color == "white") {
           this.renderer.setStyle(this.el.nativeElement, 'background-color', this.bg_color);
           this.renderer.setStyle(btn_svg, 'color', 'black');
           this.renderer.setStyle(this.el.nativeElement, 'color', '#333333');
-      }
-         else{
+        }
+
+        else{
         this.renderer.setStyle(this.el.nativeElement, 'background-color', this.bg_color);
         this.renderer.setStyle(btn_svg, 'color', 'white');
         this.renderer.setStyle(this.el.nativeElement, 'color', '#ffffff');
         this.renderer.removeStyle(this.el.nativeElement, 'opacity');
-    }
+       }
 }
 
 private accordionItemHoverStyle() {
@@ -72,34 +66,27 @@ private accordionItemHoverStyle() {
   }
 }
 
-
-
 private disable() {
-   // const accordion__item = this.el.nativeElement.querySelector('.accordion__item','.accordion__header');
- 
    this.renderer.setStyle(this.el.nativeElement, 'display', 'flex'); 
    this.renderer.setStyle(this.el.nativeElement, 'padding', '0 1rem'); 
    this.renderer.setStyle(this.el.nativeElement, 'justify-content', 'space-between'); 
    this.renderer.setStyle(this.el.nativeElement, 'align-items', 'center'); 
    this.renderer.setStyle(this.el.nativeElement, 'border', '1px solid #B5B5B5');   
    this.renderer.setStyle(this.el.nativeElement, 'font-size', '18px'); 
-
-
-    this.renderer.setStyle(this.el.nativeElement, 'opacity', '0.8');
-    this.renderer.setStyle(this.el.nativeElement, 'cursor', 'not-allowed');
-    const btn_svg = this.el.nativeElement.querySelector('.btn_svg');
+   this.renderer.setStyle(this.el.nativeElement, 'opacity', '0.8');
+   this.renderer.setStyle(this.el.nativeElement, 'cursor', 'not-allowed');
+   const btn_svg = this.el.nativeElement.querySelector('.btn_svg');
    
     if(this.bg_color == "white") {
       this.renderer.setStyle(this.el.nativeElement, 'background-color', this.bg_color);
       this.renderer.setStyle(btn_svg, 'color', 'black');
       this.renderer.setStyle(this.el.nativeElement, 'color', '#333333');
-  }
-     else{
-    this.renderer.setStyle(this.el.nativeElement, 'background-color', this.bg_color);
-    this.renderer.setStyle(btn_svg, 'color', 'white');
-    this.renderer.setStyle(this.el.nativeElement, 'color', '#ffffff');
-   // this.renderer.removeStyle(this.el.nativeElement, 'opacity');
-}
-  
+    }
+
+    else{
+      this.renderer.setStyle(this.el.nativeElement, 'background-color', this.bg_color);
+      this.renderer.setStyle(btn_svg, 'color', 'white');
+      this.renderer.setStyle(this.el.nativeElement, 'color', '#ffffff');
+    } 
   }
 }
