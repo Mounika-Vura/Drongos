d6e12153161f0847b3af75e5917965be1b39d32c
NiharMohanty671 <nihar.mohanty@techwave.net>
2024-06-07 13:23:06 +0530

    Layout Branch 07 Modified

---
+++ src/app/components/layouts/empty-box-layout/empty-box-layout.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-empty-box-layout',
+  templateUrl: './empty-box-layout.component.html',
+  styleUrl: './empty-box-layout.component.scss'
+})
+export class EmptyBoxLayoutComponent {
+  width = 700;
+  height = 500;
+
+}
